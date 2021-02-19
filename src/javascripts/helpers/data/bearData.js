7c6221f39c4e58c9c66e44a5e2c573b1d01f9b93
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-02-19 11:26:35 -0600

    new images innerHTML

--- src/javascripts/helpers/data/bearData.js
+++ src/javascripts/helpers/data/bearData.js
@@ -2,39 +2,28 @@ const bears = [
   {
     id: '1',
     name: 'Ice Queen',
-    imageURL: 'https://user-images.githubusercontent.com/67122062/108262446-90fd0c00-712a-11eb-93cb-27def7d430eb.png',
-    fishCounter: 0
+    imageURL: 'https://i2.wp.com/churchillwild.com/wp-content/uploads/2019/03/janet-the-polar-bear-nanuk-polar-bear-lodge-george-turner-photo.jpg',
   },
   {
     id: '2',
     name: 'HBIC',
-    imageURL: 'https://user-images.githubusercontent.com/67122062/108262456-935f6600-712a-11eb-8028-acfc05d21f02.png',
-    fishCounter: 0
+    imageURL: 'https://i.pinimg.com/736x/31/86/32/318632830994e5fca3840f031c4d202e.jpg',
   },
   {
     id: '3',
     name: 'Poseidon',
-    imageURL: 'https://user-images.githubusercontent.com/67122062/108262465-965a5680-712a-11eb-8706-3aa63f697f9a.png',
-    fishCounter: 0
+    imageURL: 'https://i.pinimg.com/originals/c7/68/1e/c7681ea82214ea4ddbd0bd4f50ffae60.jpg',
   },
   {
     id: '4',
     name: 'Miss Congeniality',
-    imageURL: 'https://user-images.githubusercontent.com/67122062/108262477-99eddd80-712a-11eb-8a03-86b2a95e0866.png',
-    fishCounter: 0
+    imageURL: 'https://i.pinimg.com/736x/d5/be/61/d5be61165d3e2940e08a9fb7a9c5912e.jpg',
   },
   {
     id: '5',
     name: 'Sleeper Hit',
-    imageURL: 'https://user-images.githubusercontent.com/67122062/108262492-9d816480-712a-11eb-9343-cf1bb3222385.png',
-    fishCounter: 0
+    imageURL: 'https://i.pinimg.com/originals/dc/65/92/dc6592a1b3cc8fba23b682f2dc0cd908.jpg',
   }
 ];
 
-const findBears = () => bears;
-
-const newBear = (obj) => {
-  bears.push(obj);
-};
-
-export default { bears, newBear, findBears };
+export default { bears };
--- src/javascripts/helpers/river.js
+++ src/javascripts/helpers/river.js
@@ -4,20 +4,19 @@ const buildABear = (arr) => {
   let domString = '';
   for (let i = 0; i < arr.length; i += 1) {
     domString += `<div class='card my-2' style='width: 18rem;' id=${i}>
-                    <div class='img-container' style='background-image: url('${arr.imageUrl}' id='imageUrl');'></div>
                     <div class='card-body'>
-                      <p class='card-text' id='name'>${arr.name}</p>
-                      <p class='card-text' id='fishCounter'>${arr.fishCounter}</p>
+                      <img src='${arr[i].imageURL}' style='height:250px;width: 225px'>
+                      <p class='card-text' id='name'>${arr[i].name}</p>
                       <div class='card-footer' id='event-buttons'>                                                                           
-                      <button type='button' class='btn-attempt' id='attempt-${i}'>Attempts:</button>
-                      <button type='button' class='btn-catch' id='catch-${i}'>Catches:</button>
+                      <button type='button' class='btn-attempt' id='attempt-${i}'>Attempt</button>
+                      <button type='button' class='btn-catch' id='catch-${i}'>Catch</button>
                       </div>
                       <div id='total-events-${i}'></div>
                     </div>
                   </div>`;
   }
 
-  printToDOM('#bear-cards', domString);
+  printToDOM('bear-cards', domString);
 };
 
 export default buildABear;
