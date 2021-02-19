95c7819ac62eebe0f0168bd154d995185652e6d1
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-02-19 11:29:55 -0600

    new images

--- src/javascripts/helpers/river.js
+++ src/javascripts/helpers/river.js
@@ -5,7 +5,7 @@ const buildABear = (arr) => {
   for (let i = 0; i < arr.length; i += 1) {
     domString += `<div class='card my-2' style='width: 18rem;' id=${i}>
                     <div class='card-body'>
-                      <img src='${arr[i].imageURL}' style='height:250px;width: 225px'>
+                      <img src='${arr[i].imageURL}' style='height:250px;width:225px'>
                       <p class='card-text' id='name'>${arr[i].name}</p>
                       <div class='card-footer' id='event-buttons'>                                                                           
                       <button type='button' class='btn-attempt' id='attempt-${i}'>Attempt</button>
